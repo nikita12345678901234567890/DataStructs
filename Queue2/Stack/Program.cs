5fb7a6b98db5d9676b4bf4abb026121d10022e64
Nikita Ulianov <Nikita.Ulianov@GMR.local>
2020-06-10 17:27:04 -0700

    NO

---
+++ Queue2/Stack/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fish<bool> stuck = new fish<bool>();
+            stuck.Enqueue(false);
+            stuck.Enqueue(true);
+            Console.WriteLine(stuck.Peek().ToString());
+            Console.WriteLine(stuck.Dequeue().ToString());
+            Console.WriteLine(stuck.Dequeue().ToString());
+            stuck.BigRedButton();
+        }
+    }
+}
---
+++ Queue2/Stack/fish.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Stack
+{
+    class fish<T> where T : IComparable
+    {
+        List<T> a_list = new List<T>();
+        public fish()
+        {
+            Console.WriteLine("fish");
+        }
+
+        public void Enqueue (T thing)
+        {
+            a_list.Add(thing);
+        }
+
+        public T Peek()
+        {
+            return a_list[0];
+        }
+
+        public void BigRedButton()
+        {
+            a_list.Clear();
+        }
+
+        public T Dequeue()
+        {
+            T thing = Peek();
+            a_list.RemoveAt(0);
+            return thing;
+        }
+    }
+}
