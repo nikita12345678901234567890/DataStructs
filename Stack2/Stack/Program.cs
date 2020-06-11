553b69a27e2486d3003cf4116349674f0aff13f1
Nikita Ulianov <Nikita.Ulianov@GMR.local>
2020-06-10 17:16:02 -0700

    did assignment

---
+++ Stack2/Stack/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stuck<bool> stuck = new Stuck<bool>();
+            stuck.Push(false);
+            stuck.Push(true);
+            Console.WriteLine(stuck.Peek().ToString());
+            stuck.Popopopopopopopopopopopopop();
+            stuck.BigRedButton();
+        }
+    }
+}
---
+++ Stack2/Stack/Stuck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Stack
+{
+    class Stuck<T> where T : IComparable
+    {
+        List<T> a_list = new List<T>();
+        public Stuck()
+        {
+            Console.WriteLine("fish");
+        }
+
+        public void Push(T thing)
+        {
+            a_list.Add(thing);
+        }
+
+        public T Peek()
+        {
+            return a_list[a_list.Count - 1];
+        }
+
+        public void BigRedButton()
+        {
+            a_list.Clear();
+        }
+
+        public T Popopopopopopopopopopopopop()
+        {
+            T thing = Peek();
+            a_list.RemoveAt(a_list.Count - 1);
+            return thing;
+        }
+    }
+}
