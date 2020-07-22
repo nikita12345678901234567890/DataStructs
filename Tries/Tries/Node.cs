059c962c5f8ac1e40ffd296bf1b36c85e48172d5
Nikita Ulianov <Nikita.Ulianov@GMR.local>
2020-07-21 17:31:08 -0700

    finish this

---
+++ Tries/Tries/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tries
+{
+    class Node
+    {
+        public char letter;
+        public Dictionary<char, Node> Children;
+        public bool end;
+
+        public Node(char letter)
+        {
+            this.letter = letter;
+            Children = new Dictionary<char, Node>();
+            end = false;
+        }
+        public Node(char letter, bool end)
+        {
+            this.letter = letter;
+            Children = new Dictionary<char, Node>();
+            this.end = end;
+        }
+    }
+}
---
+++ Tries/Tries/Trie.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tries
+{
+    class Tire
+    {
+        Node root;
+
+        public Tire()
+        {
+            root = new Node('|');
+        }
+        public void Clear()
+        {
+            root.Children.Clear();
+        }
+
+        public void Insert(string word)
+        {
+            Node fish = root;
+            var Word = word.ToCharArray();
+
+            for (int i = 0; i < Word.Length; i++)
+            {
+                if (fish.Children.ContainsKey(Word[i]))
+                {
+                    fish = fish.Children[Word[i]];
+                }
+                else
+                {
+                    fish.Children.Add(Word[i], new Node(Word[i]));
+                }
+            }
+        }
+
+        public bool Contains(string word)
+        { 
+            
+        }
+
+        private Node Search(string word)
+        { 
+            
+        }
+
+        public List<string> GetAllMatchingPrefix(string prefix)
+        {
+
+        }
+
+        public bool Remove(string prefix)
+        { 
+            
+        }
+    }
+}
