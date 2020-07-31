9468dc2b7deff82f746837ac826005cee2d8d7e6
Nikita Ulianov <Nikita.Ulianov@GMR.local>
2020-07-30 17:33:36 -0700

    finish testing

---
+++ BloomFilter/BloomFilter/FilterBloom.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloomFilter
+{
+    class FilterBloom<T>
+    {
+        List<Func<T, int>> funcS;
+        int M = 0;
+        int N = 0;
+        int K = 3;
+        bool[] array;
+
+        public FilterBloom(int cap)
+        {
+            M = cap;
+            array = new bool[M];
+            funcS = new List<Func<T, int>>();
+            funcS.Add(HashFuncOne);
+            funcS.Add(HashFuncTwo);
+            funcS.Add(HashFuncThree);
+        }
+
+        public void LoadHashFunc(Func<T, int> hashFunc)
+        {
+            funcS.Add(hashFunc);
+            K++;
+        }
+
+        public void Insert(T item)
+        {
+            for (int i = 0; i < K; i++)
+            {
+                array[Math.Abs(funcS[i](item)) % array.Length] = true;
+            }
+            N++;
+        }
+
+        public bool ProbablyContains(T item)
+        {
+            bool yoot = true;
+            for (int i = 0; i < K; i++)
+            {
+                if (array[Math.Abs(funcS[i](item)) % array.Length] == false)
+                {
+                    return false;
+                }
+            }
+            
+            return yoot;
+        }
+
+        private int HashFuncOne(T item)
+        {
+            return item.GetHashCode();
+        }
+
+        private int HashFuncTwo(T item)
+        {
+            return HashFuncThree(item) + "tempString".GetHashCode();
+        }
+
+        private int HashFuncThree(T item)
+        {
+            return int.Parse(HashFuncOne(item).ToString()).GetHashCode();
+        }
+    }
+}
---
+++ BloomFilter/BloomFilter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BloomFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            FilterBloom<int> BoomFilter = new FilterBloom<int>(100);
+            List<int> yeet = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                int yoot = random.Next(int.MinValue, int.MaxValue);
+                BoomFilter.Insert(yoot);
+                if (i % 10 == 0)
+                {
+                    yeet.Add(yoot);
+                }
+            }
+            for (int i = 0; i < yeet.Count; i++)
+            {
+                Console.WriteLine(BoomFilter.ProbablyContains(yeet[i]));
+            }
+            Console.WriteLine(BoomFilter.ProbablyContains(150));
+            
+        }
+    }
+}
