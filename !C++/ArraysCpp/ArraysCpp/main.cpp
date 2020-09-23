134d8ef62d8901b4fd061b67d2fc5717b117c3b0
Nikita Ulianov <Nikita.Ulianov@GMR.local>
2020-09-22 17:02:53 -0700

    finnish this at home

---
+++ !C++/ArraysCpp/ArraysCpp/main.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <array>
+#include <string>
+#include <cmath>
+
+int sum(int* numbers, int size)
+{
+	int sumval = 0;
+
+	for (size_t i = 0; i < size; i++)
+	{
+		sumval += numbers[i];
+	}
+
+	return sumval;
+}
+
+// pass by pointer
+void swap(int* x, int* y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+
+// pass by reference
+void swap(int& x, int& y)
+{
+	int temp = x;
+	x = y;
+	y = temp;
+}
+
+int hecDec(std::string hex)
+{
+	int thing = 0;
+	int count = 0;
+	for (char c : hex)
+	{
+		int temp = 0;
+		if (c >= 0 && c < 10)
+		{
+			temp = c;
+		}
+		else
+		{
+			switch (c)
+			{
+			case 'a':
+				temp = 10;
+				break;
+			case 'b':
+				temp = 11;
+				break;
+			case 'c':
+				temp = 12;
+				break;
+			case 'd':
+				temp = 13;
+				break;
+			case 'e':
+				temp = 14;
+				break;
+			case 'f':
+				temp = 15;
+				break;
+			}
+		}
+
+		thing += temp * std::pow(16, count);
+
+		count++;
+	}
+	return thing;
+}
+
+int main()
+{
+
+	//int numbers[3];	// allocate an array of length 3 on the stack
+
+
+	//const int size = 3;
+
+	//int arr[size];
+
+
+	/*int numbers[] = { 5,6,3,3 };
+	for (auto& num : numbers)
+	{
+		std::cout << num << std::endl;
+	}
+	*/
+
+	//int x = 5;
+
+	//int* xpointer = &x;
+
+
+	//std::cout << x << std::endl;
+
+	//std::cout << xpointer << std::endl;
+
+	//std::cout << *xpointer << std::endl;		// * before pointer variable is called dereferencing the pointer (since pointer is a reference), give me the value at that address
+
+
+	//std::cout << &xpointer << std::endl;
+
+	//int** xpointerpointer = &xpointer;
+
+	//*xpointer = 6;
+
+
+	/*int numbers[] = { 5,6,-1 };
+
+	std::cout << sum(numbers, 3) << std::endl;*/
+
+
+	/*for (size_t i = 0; i < 3; i++)
+	{
+		std::cout << numbers[i] << " = " << &numbers[i] << std::endl;
+	}
+
+
+	std::cout << &numbers << std::endl;
+
+
+	int* numberarray = numbers;
+
+	std::cout << numberarray << std::endl;
+	std::cout << *numberarray << std::endl;*/
+
+
+	//std::array<int, 3> numbers = { 4,5,6 };
+
+
+	//// size_t is shorthand for unsigned int
+
+	//for(size_t i = 0; i < numbers.size(); i++)
+	//{
+	//	std::cout << numbers[i] << std::endl;
+	//}
+
+
+	// ask the user for 5 numbers and store them in an array, print out their valuse and their memory address
+
+	/*std::array<int, 5> numbers{};
+
+	for (size_t i = 0; i < numbers.size(); i++)
+	{
+		int val = 0;
+		std::cin >> val;
+
+		numbers[i] = val;
+	}
+	for (size_t i = 0; i < numbers.size(); i++)
+	{
+		std::cout << numbers[i] << "\n" << &numbers[i] << "\n";
+	}*/
+
+
+	// create a function called swap, that takes in two int pointers and swaps their values
+
+	//int x = 5;
+	//int y = 7;
+
+	////swap(&x, &y);
+	//swap(x, y);
+
+	//std::cout << x << "\n" << y << "\n";
+
+	// ask the user for a hex string, convert it to decimal
+
+	std::string thing;
+	std::cin >> thing;
+
+	std::cout << hecDec(thing);
+
+	return 0;
+}
