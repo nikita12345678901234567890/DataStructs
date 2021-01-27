4709936c9ad84cee8dd1eb2beb6086f0c91e08cf
Nikita Ulianov <Nikita.Ulianov@GMR.local>
2021-01-26 16:59:14 -0800

    do this

---
+++ Huffman/Huffman/Coder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Huffman
+{
+    class Coder
+    {
+        public Node Root;
+
+        public Coder()
+        {
+            
+        }
+
+
+        public string Encode(string text)
+        {
+            Dictionary<char, int> frequency = new Dictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!frequency.ContainsKey(text[i]))
+                {
+                    frequency.Add(text[i], 1);
+                }
+                else
+                {
+                    frequency[text[i]]++;
+                }
+            }
+
+            //make tree!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  
+        }
+    }
+}
---
+++ Huffman/Huffman/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Huffman
+{
+    class Node
+    {
+        public char character;
+        public Node Rchild;
+        public Node Lchild;
+
+        public Node(char character)
+        {
+            this.character = character;
+        }
+    }
+}
