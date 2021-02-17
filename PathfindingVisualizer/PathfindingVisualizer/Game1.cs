f1076071f962aa922151136cd7a98ba3fecb9a22
Nikita Ulianov <Nikita.Ulianov@GMR.local>
2021-02-16 17:05:12 -0800

    refreshed monogame

---
+++ PathfindingVisualizer/PathfindingVisualizer/Game1.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using System;
+using System.Collections.Generic;
+
+namespace PathfindingVisualizer
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        List<Sprite> sprites;
+        Random random = new Random();
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            IsMouseVisible = true;
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            sprites = new List<Sprite>();
+
+            var texture = Content.Load<Texture2D>("circle");
+
+            for (int i = 0; i < 4; i++)
+            {
+                sprites.Add(new Sprite(texture, new Vector2(random.Next(0, GraphicsDevice.Viewport.Width), random.Next(0, GraphicsDevice.Viewport.Height)), Vector2.One, new Vector2(texture.Width / 2, texture.Height / 2)));
+            }
+
+            // TODO: use this.Content to load your game content here
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+
+            for (int i = 0; i < 4; i++)
+            {
+                sprites[i].Draw(spriteBatch);
+            }
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ PathfindingVisualizer/PathfindingVisualizer/Sprite.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathfindingVisualizer
+{
+    class Sprite
+    {
+        public Texture2D texture { get; set; }
+        public Vector2 position { get; set; }
+        public Vector2 origin { get; set; }
+        public Vector2 scale{ get; set; }
+
+        public float rotation { get; set; }
+        public SpriteEffects effect { get; set; }
+        public float layerDepth { get; set; }
+
+        public Sprite(Texture2D texture, Vector2 position, Vector2 scale, Vector2 origin)
+        {
+            this.texture = texture;
+            this.origin = origin;
+
+            this.position = position;
+            this.scale = scale;
+        }
+
+        public void Update()
+        { 
+            
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, null, Color.CornflowerBlue, rotation, origin, scale, effect, layerDepth);
+        }
+    }
+}
